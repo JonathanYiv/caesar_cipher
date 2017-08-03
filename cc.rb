52963eeac0230ecb9d3c7fe322dd548ff71c78a7
Jonathan Yiv <JonathanYiv@gmail.com>
2017-08-03 15:32:34 -0400

    Created a simple caesar cipher

--- cc.rb
+++ cc.rb
@@ -1,3 +1,18 @@
-def caesar_cipher string
-	
+def caesar_cipher string, number
+	for i in 0...string.length
+		if(string[i].match(/\w/))
+			for j in 1..number
+				if(string[i] == "z")
+					string[i] = "a"
+				elsif string[i] == "Z"
+					string[i] = "A"
+				else
+					string[i] = string[i].next
+				end
+			end
+		end
+	end
+	puts string
 end
+
+caesar_cipher("What a string!", 5)
